bce1d564e9e92d3b5f6432c694c0f916852b21f2
Nicholas <infinitevolume@gmail.com>
2023-06-15 15:32:38 -0700

    added logo and nav items

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,26 @@ import {
   StarsCanvas,
 } from './components';
 
-function App() {
+const App = () => {
   return (
-    <div className='relative z-0 bg-primary'>
-      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-        <Navbar />
-        <Hero />
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
-      <About />
-      <Experience />
-      <Tech />
-      <Works />
-      <Feedbacks />
-      <div className='relative z-0'>
-        <Contact />
-        <StarsCanvas />
-      </div>
-    </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
